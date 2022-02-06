3e3d0eb33f66f81df7067a94d18cc4cc6192b61c
Vishal Agarwal <agarwaldel20@gmail.com>
2022-02-07 00:01:42 +0530

    Created Bitmex Module
    Create bitmex module and API call to get trading pairs

---
+++ bitmex.py
@@ -0,0 +1,13 @@
+import requests
+
+def get_contracts():
+    response_object = requests.get("https://www.bitmex.com/api/v1/instrument/active")
+
+    contracts = []
+
+    for contract in response_object.json():
+        contracts.append(contract['symbol'])
+
+    return contracts
+
+print(get_contracts())
