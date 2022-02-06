622b1e362b2d9f28fecb00900162046ffdf77039
Vishal Agarwal <agarwaldel20@gmail.com>
2022-02-07 00:16:19 +0530

    DIsplaying API data
    displaying API data and formatting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import logging
+from bitmex import get_contracts
 
 logger = logging.getLogger()
 
@@ -18,6 +19,26 @@ file_handler.setLevel(logging.DEBUG)
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 
+i=0
+j=0
+
 if __name__ == "__main__":
+
+    bitmex_contracts = get_contracts()
+
     root = tk.Tk()
+    root.configure(bg = 'gray12')
+
+    calibri_font = ("Calibri", 11, "normal")
+
+    for contracts in bitmex_contracts:
+        label_widget = tk.Label(root, text=contracts, bg='gray12', fg='SteelBlue1', width = 13, font = calibri_font )
+        label_widget.grid(row=i,column=j, sticky='ew')
+
+        if i == 4:
+            j+=1
+            i=0
+        else:
+            i+=1
+
     root.mainloop()
