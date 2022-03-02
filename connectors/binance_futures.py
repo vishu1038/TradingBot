2e66401ee0801a93534bd9ae64b41cd7aedd2841
Vishal Agarwal <agarwaldel20@gmail.com>
2022-03-02 10:40:41 +0530

    Added Websocket API
    added Websocket api and its related functions

--- connectors/binance_futures.py
+++ connectors/binance_futures.py
@@ -8,6 +8,11 @@ from urllib.parse import urlencode
 
 import requests
 
+import websocket
+import json
+
+import threading
+
 logger = logging.getLogger()
 
 
@@ -15,8 +20,10 @@ class BinanceFuturesClient:
     def __init__(self, public_key, secret_key, testnet):
         if testnet:
             self.base_url = "https://testnet.binancefuture.com"
+            self.wss_url = "wss://stream.binancefuture.com/ws"
         else:
             self.base_url = "https://fapi.binance.com"
+            self.wss_url = "wss://fstream.binance.com/ws"
 
         self.public_key = public_key
         self.secret_key = secret_key
@@ -24,6 +31,12 @@ class BinanceFuturesClient:
         self.headers = {'X-MBX-APIKEY': self.public_key}
 
         self.prices = dict()
+
+        self.id = 1
+        self.ws = None
+
+        t = threading.Thread(target=self.start_ws)
+        t.start()
 
         logger.info("Binance Futures Client successfully Initialized")
 
@@ -90,7 +103,7 @@ class BinanceFuturesClient:
 
     def get_balances(self):
         data = dict()
-        data['timestamp'] = int(time.time()*1000)
+        data['timestamp'] = int(time.time() * 1000)
         data['signature'] = self.generate_signature(data)
 
         balances = dict()
@@ -127,7 +140,7 @@ class BinanceFuturesClient:
 
         data = dict()
         data['orderId'] = order_id
-        data['symbol']  = symbol
+        data['symbol'] = symbol
 
         data['timestamp'] = int(time.time() * 1000)
         data['signature'] = self.generate_signature(data)
@@ -148,4 +161,53 @@ class BinanceFuturesClient:
 
         return order_status
 
+    def start_ws(self):
+        self.ws = websocket.WebSocketApp(self.wss_url, on_open=self.on_open, on_close=self.on_close, on_error=self.on_error,
+                                    on_message=self.on_message)
+        self.ws.run_forever()
+        return
 
+    def on_open(self, ws):
+        logger.info("Binance connection opened")
+
+        self.subscribe_channel("BTCUSDT")
+        return
+
+    def on_close(self, ws):
+        logger.warning("Binance connection closed")
+        return
+
+    def on_error(self, ws, msg):
+        logger.error("Binance connection error: %s", msg)
+        return
+
+    def on_message(self, ws, msg):
+
+        data = json.loads(msg)
+
+        if "e" in data:
+            if data['e'] == "bookTicker":
+
+                symbol = data['s']
+
+                if symbol not in self.prices:
+                    self.prices[symbol] = {'bid': float(data['b']), 'ask': float(data['a'])}
+                else:
+                    self.prices[symbol]['bid'] = float(data['b'])
+                    self.prices[symbol]['ask'] = float(data['a'])
+
+                print(self.prices[symbol])
+
+
+    def subscribe_channel(self, symbol):
+        data = dict()
+        data['method'] = "SUBSCRIBE"
+        data['params'] = []
+        data['params'].append(symbol.lower() + "@bookTicker")
+        data['id'] = self.id
+
+        self.ws.send(json.dumps(data))
+
+        self.id += 1
+
+        return
