a9b6a28eb263ab992df3a6c439f08497ba5fc449
Vishal Agarwal <agarwaldel20@gmail.com>
2022-05-01 01:01:14 +0530

    Adding Updating Prices in watchlist
    added the update prices function which updates the prices in the watchlist table

--- interface/root_component.py
+++ interface/root_component.py
@@ -1,4 +1,6 @@
+import logging
 import tkinter as tk
+import logging
 
 from connectors.bitmex import BitmexClient
 from connectors.binance_futures import BinanceFuturesClient
@@ -6,6 +8,10 @@ from connectors.binance_futures import BinanceFuturesClient
 from interface.styling import *
 from interface.logging_component import Logging
 from interface.watchlist_component import Watchlist
+
+
+logger = logging.getLogger()
+
 
 
 class Root(tk.Tk):
@@ -35,6 +41,8 @@ class Root(tk.Tk):
 
     def _update_ui(self):
 
+        # Logs
+
         for log in self.bitmex.logs:
             if not log['displayed']:
                 self._logging_frame.add_log(log['log'])
@@ -45,4 +53,48 @@ class Root(tk.Tk):
                 self._logging_frame.add_log(log['log'])
                 log['displayed'] = True
 
+        # WatchList Prices
+
+        try:
+            for key, value in self._watchlist_frame.body_widgets['symbol'].items():
+
+                symbol = self._watchlist_frame.body_widgets['symbol'][key].cget("text")
+                exchange = self._watchlist_frame.body_widgets['exchange'][key].cget("text")
+
+                if exchange == "Binance":
+                    if symbol not in self.binance.contracts:
+                        continue
+
+                    if symbol not in self.binance.prices:
+                        self.binance.get_bid_ask(self.binance.contracts[symbol])
+                        continue
+
+                    precision = self.binance.contracts[symbol].price_decimals
+
+                    prices = self.binance.prices[symbol]
+
+                elif exchange == "Bitmex":
+                    if symbol not in self.bitmex.contracts:
+                        continue
+
+                    if symbol not in self.bitmex.prices:
+                        continue
+
+                    precision = self.bitmex.contracts[symbol].price_decimals
+
+                    prices = self.bitmex.prices[symbol]
+
+                else:
+                    continue
+
+                if prices['bid'] is not None:
+                    price_str = "{0:.{prec}f}".format(prices['bid'], prec=precision)
+                    self._watchlist_frame.body_widgets['bid_var'][key].set(price_str)
+
+                if prices['ask'] is not None:
+                    price_str = "{0:.{prec}f}".format(prices['ask'], prec=precision)
+                    self._watchlist_frame.body_widgets['ask_var'][key].set(price_str)
+
+        except RuntimeError as e:
+            logger.error("Error while looping through the watchlist dictionary: %s", e)
         self.after(1500, self._update_ui)
--- interface/watchlist_component.py
+++ interface/watchlist_component.py
@@ -40,10 +40,10 @@ class Watchlist(tk.Frame):
 
         self.body_widgets = dict()
 
-        self._headers = ["symbol", "exchange", "bid", "ask"]
+        self._headers = ["symbol", "exchange", "bid", "ask", "remove"]
 
         for idx, h in enumerate(self._headers):
-            header = tk.Label(self._table_frame, text=h.capitalize(), bg=BG_COLOR, fg=FG_COLOR, font=BOLD_FONT)
+            header = tk.Label(self._table_frame, text=h.capitalize() if h != "remove" else "", bg=BG_COLOR, fg=FG_COLOR, font=BOLD_FONT)
             header.grid(row=0, column=idx)
 
         for h in self._headers:
@@ -52,6 +52,12 @@ class Watchlist(tk.Frame):
                 self.body_widgets[h + "_var"] = dict()
 
         self._body_index = 1
+
+    def _remove_symbol(self, b_index:int):
+
+        for h in self._headers:
+            self.body_widgets[h][b_index].grid_forget()
+            del self.body_widgets[h][b_index]
 
     def _add_binance_symbol(self, event):
         symbol = event.widget.get()
@@ -89,4 +95,8 @@ class Watchlist(tk.Frame):
                                                      font=GLOBAL_FONT)
         self.body_widgets['ask'][b_index].grid(row=b_index, column=3)
 
-        return
+        self.body_widgets['remove'][b_index] = tk.Button(self._table_frame, text="x", bg="darkred", fg=FG_COLOR, font=GLOBAL_FONT, command=lambda: self._remove_symbol(b_index))
+        self.body_widgets['remove'][b_index].grid(row=b_index, column=4)
+
+        self._body_index +=1
+
